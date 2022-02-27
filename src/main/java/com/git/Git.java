03e4169026509580c63a5e0962de5859c6eefadc
luoz <1121178715@qq.com>
2022-02-27 09:12:06 +0800

    push-test-commit

--- src/main/java/com/git/Git.java
+++ src/main/java/com/git/Git.java
@@ -6,5 +6,6 @@ public class Git {
         System.out.println("hello git2");
         System.out.println("hello master");
         System.out.println("hot-fix-test");
+        System.out.println("hot-fix-test");
     }
 }
